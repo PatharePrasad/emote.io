b3eeaafefa2e75bf993ac8c483a52cbe23c5e5d8
Prasad Pathare <prasadsachinist10@gmail.com>
2025-02-02 12:23:31 +0000

    socket.io

---
+++ client/components/Emote.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import { io } from "socket.io-client";
+import { Card } from "@heroui/card";
+import { Button } from "@heroui/button";
+
+const socket = io(
+  "https://cuddly-space-memory-gv577p4qgqx3w5pg-8000.app.github.dev/"
+);
+
+function Emote() {
+  const [emoji, setEmoji] = useState("😏");
+
+  return (
+    <main className="min-h-screen flex items-center justify-center gap-4 flex-col">
+      <h1 className="text-3xl lowercase font-bold">emote.io</h1>
+      <Card className="text-6xl p-3">{emoji}</Card>
+      <ShowEmotes setEmoji={setEmoji} />
+    </main>
+  );
+}
+
+function ShowEmotes({ setEmoji }) {
+  const emojis = ["😀", "😁", "🤣", "😂", "😎", "🙄"];
+
+  const handleBtnClick = (emoji) => {
+    setEmoji(emoji);
+    socket.emit("emoji", emoji);
+  };
+
+  return (
+    <div className="flex gap-2 flex-wrap justify-center">
+      {emojis.map((emoji) => (
+        <Button
+          key={emoji}
+          variant="flat"
+          onPress={() => handleBtnClick(emoji)}
+        >
+          <p className="text-xl">{emoji}</p>
+        </Button>
+      ))}
+    </div>
+  );
+}
+
+export default Emote;
